cd3e63d5dc2652fbfd789af3d35521bfa63348db
austid <austid@taltech.ee>
2026-04-27 21:57:56 +0300

    updated script parameters to include temp, swap full/eval switch

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from typing import Tuple
 
 # default configurations
-MODEL_TEMP = 0
 MODEL_TOP_P = 0.9
 MODEL_NUM_PREDICT = 2048
 # partial battery for screening
@@ -13,12 +12,12 @@ MODEL_NUM_PREDICT = 2048
 PARTIAL_SCREENING = [0, 1, 3, 4, 7, 10, 12]
 
 
-def response_chat(llm_model: str, query: str) -> Tuple[str, str]:
+def response_chat(llm_model: str, temperature: float, query: str) -> Tuple[str, str]:
     response = ollama.chat(
         model=llm_model,
         messages=[{ 'role': 'user', 'content': query }],
         options={
-            'temperature': MODEL_TEMP,
+            'temperature': temperature,
             'top_p': MODEL_TOP_P,
             'num_predict': MODEL_NUM_PREDICT,
         },
@@ -27,12 +26,12 @@ def response_chat(llm_model: str, query: str) -> Tuple[str, str]:
     return response['message']['content'], response['message']['thinking']
 
 
-def stream_chat(llm_model: str, query: str) -> Tuple[str, str]:
+def stream_chat(llm_model: str, temperature: float, query: str) -> Tuple[str, str]:
     stream = ollama.chat(
         model=llm_model,
         messages=[{'role': 'user', 'content': query}],
         options={
-            'temperature': MODEL_TEMP,
+            'temperature': temperature,
             'top_p': MODEL_TOP_P,
             'num_predict': MODEL_NUM_PREDICT,
         },
@@ -64,10 +63,11 @@ def stream_chat(llm_model: str, query: str) -> Tuple[str, str]:
 def main():
     parser = argparse.ArgumentParser(prog='test', description="test an llm")
     parser.add_argument('-model', '-m', required=True, help="model to query")
+    parser.add_argument('-temperature', '-t', default=0.0, help="model temperature value")
     parser.add_argument('-battery', '-b', default='battery.json', help="question battery file to load")
-    parser.add_argument('--full', '-f', action='store_true', help="run full question battery, otherwise partial")
-    parser.add_argument('--preserve_thinking', '-p', action='store_true', help="preserve thinking in output file")
-    parser.add_argument('--stream', '-s', action='store_true', help="stream model response")
+    parser.add_argument('-evaluation', '-e', action='store_true', help="run full question battery, otherwise partial")
+    parser.add_argument('-preserve_thinking', '-p', action='store_true', help="preserve thinking in output file")
+    parser.add_argument('-stream', '-s', action='store_true', help="stream model response")
 
     args = parser.parse_args()
 
@@ -77,14 +77,14 @@ def main():
         battery = json.load(json_file)
 
     # set rounds of questioning to either length of battery file for full or predefined list for partial
-    rounds = list(range(0, len(battery['questions']))) if args.full else PARTIAL_SCREENING
+    rounds = PARTIAL_SCREENING if args.evaluation else list(range(0, len(battery['questions'])))
 
     # perform battery
     for r in rounds:
         question = battery['questions'][r]['question']
         print(f"\n[{datetime.now()}] ▶️ {args.model} queried: {question}")
 
-        response = stream_chat(args.model, question) if args.stream else response_chat(args.model, question)
+        response = stream_chat(args.model, float(args.temperature), question) if args.stream else response_chat(args.model, float(args.temperature), question)
 
         output = f"# Question {r + 1}: {question}\n\n"
         if args.preserve_thinking:
